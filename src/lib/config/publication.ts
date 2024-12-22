20de1c7c775162aeceff483bec071e37e6b9a0e5
dungxibo123 <votiendung0805@gmail.com>
2024-12-22 19:39:45 +0800

    add two more publications

--- src/lib/config/publication.ts
+++ src/lib/config/publication.ts
@@ -36,13 +36,53 @@ export const publications: Publication[] = [
   {
     id: "intellect",
     title: "IntellectNavigator: Enhancing Search Tools with LLMs-Powered Query Instruction",
-    tags: ["Workshop", "Idea paper"],
+    tags: ["Embedding space mapping", "NLP"],
     img: "/assets/intellect.png",
     authors: [
       {
         name: "Dung Vo",
         url: "https://dungxibo123.github.io"
       },
+    ]
+  },
+  {
+    id: "vivqa",
+		title: "From Vietnamese to English: Advancing VQA with Cross-Linguistic Mapping",
+    tags: ["Workshop", "Idea paper"],
+    img: "/assets/vivqa.png",
+    authors: [
+      {
+        name: "Dung Vo",
+        url: "https://dungxibo123.github.io"
+      },
+      {
+        name: "Tung Le",
+        url: "https://www.linkedin.com/in/tung-le-35228473/"
+      },
+      {
+        name: "Huy Tien Nguyen",
+        url: "https://scholar.google.com/citations?user=WBCZ2DcAAAAJ&hl=en&oi=ao"
+      },
+    ]
+  },
+  {
+    id: "convsint",
+		title: "ConvINT: A Semi-Structured Intention Framework for Conversational Understanding",
+    tags: ["Data", "Semi-supervised learning"],
+    img: "/assets/convsint.png",
+    authors: [
+      {
+        name: "Liang Jinggui",
+        url: "https://scholar.google.com/citations?user=fDjmQw8AAAAJ&hl=zh-CN"
+      },
+      {
+        name: "Dung Vo",
+        url: "https://dungxibo123.github.io"
+      },
+      {
+        name: "Lizi Liao",
+        url: "https://lzliao.github.io"
+      }
     ]
   }
 ]
